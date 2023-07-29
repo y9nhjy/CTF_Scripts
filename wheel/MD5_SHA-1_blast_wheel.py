6deb762105a5305ff0a89235f417477c6d28865c
y9nhjy <yqnhjy@qq.com>
2023-07-29 19:28:06 +0800

    更新MD5相关文件

--- wheel/MD5_SHA-1_blast_wheel.py
+++ wheel/MD5_SHA-1_blast_wheel.py
@@ -8,34 +8,27 @@ def check(s, md5):
 
 
 md = '5a3ebb487ad0046e52db00570339aace'
-flag = bytearray(b'Y0uReallyKn0wB4s')
-# di = b'0123456789qwertyuiopasdfghjklzxcvbnm'
-di = range(0x20, 0x7F)
-ch = [chr(i) for i in range(0x20,0x7f)]
+di = [chr(i) for i in range(0x20, 0x7f)]
+len = 4
+begin = 'Y0uReallyKn0wB4s'
+end = ''
+# md = 'c8837b23ff8aaa8a2dde915473ce0991'
+# di = '0123456789'
+# len = 6
+# begin = ''
+# end = ''
+
 
 def solve(s, idx, totallenth):
     if idx == totallenth:
-        if check(s.encode(),md):
-            print(s)
+        if check((begin + s + end).encode(), md):
+            print(begin + s + end)
             exit()
         else:
             return 0
 
-    for i in range(len(ch)):
-        solve(s + ch[i], idx + 1, totallenth)
+    for i in di:
+        solve(s + i, idx + 1, totallenth)
 
-a = 'Y0uReallyKn0wB4s'
-solve(a, len(a), len(a) + 4)
 
-# 纯数字
-# win = 0
-# for i in range(100000, 999999):
-#     s = str(i)
-#     x = hashlib.md5(s.encode())
-#     if x.hexdigest() == "c8837b23ff8aaa8a2dde915473ce0991":
-#         print("Successfully!")
-#         win = aaa
-#         print(str(i))
-#         break
-# if not win:
-#     print("Failure!")
+solve('', 0, len)
