4dd5b574881b8d01eebc4fbe9bf6a2f49b858d2b
y9nhjy <yqnhjy@qq.com>
2023-08-03 15:51:45 +0800

    更新去花指令脚本

--- idc_wheel/idc_去花指令_wheel.py
+++ idc_wheel/idc_去花指令_wheel.py
@@ -1,13 +1,15 @@
 import idc
-start_addr = 0x406300
-end_addr = 0x406E14
-l = [0x74, 0x15, 0x75, 0x13]
+
+start_addr = 0x7ae580
+end_addr = 0x7aeba8
+o = [0x33, 0xd2, 0x89, 0x90, 0x94, 0x0c, 0x00, 0x00]
+n = [0xc6, 0x80, 0x94, 0x0c, 0x00, 0x00, 0x01, 0x90]
 for addr in range(start_addr, end_addr):
-    for i in range(len(l)):
-        if idc.get_wide_byte(addr + i) != l[i]:
+    for i in range(len(o)):
+        if idc.get_wide_byte(addr + i) != o[i]:
             break
     else:
-        for i in range(19):
-            ida_bytes.patch_byte(addr + i + 4, 0x90)
-        print("[+] nop ok!!")
-
+        for i in range(len(o)):
+            ida_bytes.patch_byte(addr, n[i % len(n)])
+            addr += 1
+        print("[+] Patch Successfully!!!")
